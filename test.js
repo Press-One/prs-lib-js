cf6ea2eb3fcd775802021668267f9090981a989c
chux0519 <chuxdesign@hotmail.com>
2019-04-03 16:30:50 +0800

    test: add `file` option for testing
    also initiate should in test entry

--- test.js
+++ test.js
@@ -1,13 +1,18 @@
 'use strict';
 
 const Mocha = require('mocha');
+const chai = require('chai');
 const path = require('path');
 const argv = require('yargs').argv;
+
 const mocha = new Mocha({ grep: argv.grep || '', timeout: 10000 });
 const casePath = path.join(__dirname, '/test/');
 
+// Add `should` to Object.prototype
+chai.should();
+
 require('fs').readdirSync(casePath).forEach((file) => {
-  if (file.endsWith('.js')) {
+  if (file.endsWith('.js') && file.includes(argv.file || '')) {
     let path = `${casePath}${file}`;
     mocha.addFile(path);
   }
