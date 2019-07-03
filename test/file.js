6e53af37ceb985fcab8be570087e0cf4ec2e467d
chux0519 <chuxdesign@hotmail.com>
2019-07-03 16:06:58 +0800

    test: add test cases for jpeg and png sign

--- test/file.js
+++ test/file.js
@@ -6,8 +6,12 @@ const path = require('path');
 const utility = require('prs-utility');
 const { user, buyer } = require('../fixtures');
 const PRS = require('../lib');
+const PNG = require('pngjs').PNG;
+const jpeg = require('jpeg-js');
+const prsEnv = process.env.ENV || 'env';
+
 const client = new PRS({
-  env: 'env',
+  env: prsEnv,
   debug: true,
   privateKey: utility.recoverPrivateKey(user.keystore, user.password),
   address: user.address
@@ -18,8 +22,39 @@ let fileRId = null;
 
 let markdownFileUrl = null;
 let markdownFileUrl2 = null;
+let pngFileUrl = null;
+let jpegFileUrl = null;
 
-before(function () {
+async function randomPng (width, height, dstFname) {
+  const newfile = new PNG({ width, height });
+
+  for (let i = 0; i < width * height * 4; i++) {
+    newfile.data[i] = Math.floor(Math.random() * 256);
+  }
+  return new Promise((resolve, reject) => {
+    newfile.pack()
+      .pipe(fs.createWriteStream(dstFname))
+      .on('finish', resolve)
+      .on('error', reject);
+  });
+}
+
+function randomJpeg (width, height, dstFname) {
+  var frameData = Buffer.alloc(width * height * 4);
+  for (let i = 0; i < width * height * 4; i++) {
+    frameData[i] = Math.floor(Math.random() * 256);
+  }
+  const rawImageData = {
+    data: frameData,
+    width: width,
+    height: height
+  };
+  const jpegImageData = jpeg.encode(rawImageData, 80);
+
+  fs.writeFileSync(dstFname, jpegImageData.data);
+}
+
+before(async function () {
   const markdownFile = `../${String(Date.now())}_file.md`;
   markdownFileUrl = path.join(__dirname, markdownFile);
   fs.writeFileSync(markdownFileUrl, String(Date.now() + 'file'), 'utf-8');
@@ -27,11 +62,19 @@ before(function () {
   const markdownFile2 = `../${String(Date.now())}_file2.md`;
   markdownFileUrl2 = path.join(__dirname, markdownFile2);
   fs.writeFileSync(markdownFileUrl2, String(Date.now() + 'file2'), 'utf-8');
+
+  pngFileUrl = path.join(__dirname, `../${String(Date.now())}_pic1.png`);
+  await randomPng(50, 50, pngFileUrl);
+
+  jpegFileUrl = path.join(__dirname, `../${String(Date.now())}_pic2.jpeg`);
+  await randomJpeg(50, 50, jpegFileUrl);
 });
 
 after(function () {
   fs.unlinkSync(markdownFileUrl);
   fs.unlinkSync(markdownFileUrl2);
+  fs.unlinkSync(pngFileUrl);
+  fs.unlinkSync(jpegFileUrl);
 });
 
 describe('File', function () {
@@ -49,12 +92,36 @@ describe('File', function () {
     }
   });
 
+  it('sign png file', async function () {
+    this.timeout(1000 * 200);
+    try {
+      const stream = fs.createReadStream(pngFileUrl);
+      const data = { stream: stream, filename: 'test.png', title: 'xxx' };
+      const res = await client.file.signByStream(data);
+      res.status.should.equal(200);
+    } catch (err) {
+      assert.fail(err);
+    }
+  });
+
+  it('sign jpeg file', async function () {
+    this.timeout(1000 * 200);
+    try {
+      const stream = fs.createReadStream(jpegFileUrl);
+      const data = { stream: stream, filename: 'test2.jpeg', title: 'axxx' };
+      const res = await client.file.signByStream(data);
+      res.status.should.equal(200);
+    } catch (err) {
+      assert.fail(err);
+    }
+  });
+
   it('sign file with meta', async function () {
     this.timeout(1000 * 200);
     try {
       const stream = fs.createReadStream(markdownFileUrl2);
-      let data = { stream: stream, filename: 'text.md', title: 'xxx' };
-      let meta = { uuid: 'xxxxxxx' };
+      const data = { stream: stream, filename: 'text.md', title: 'xxx' };
+      const meta = { uuid: 'xxxxxxx' };
       const res = await client.file.signByStream(data, meta);
       res.status.should.equal(200);
     } catch (err) {
@@ -82,7 +149,7 @@ describe('File', function () {
 
   it('reward', async function () {
     try {
-      const buyClient = new PRS({ env: 'env', debug: true, privateKey: utility.recoverPrivateKey(buyer.keystore, buyer.password), address: buyer.address });
+      const buyClient = new PRS({ env: prsEnv, debug: true, privateKey: utility.recoverPrivateKey(buyer.keystore, buyer.password), address: buyer.address });
       const res = await buyClient.file.reward(fileRId, 1, { comment: 'comment', memo: 'memo' });
       res.status.should.equal(200);
     } catch (err) {
